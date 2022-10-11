c89ed77d0b9fc42fe4fb9618b254309aa651303e
crimtin <christianmck@hotmail.com>
2022-10-10 22:50:21 -0500

    added cards and styles for cards and input. added Job Type.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
+
 import Card from "react-bootstrap/Card";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+
 import "./App.scss";
+import { Job } from "./types/Job";
 
 function App() {
-  const [jobData, setJobData] = useState([]);
-  // const [jobName, setJobName] = useState([]);
-  // const [jobTitle, setJobTitle] = useState([]);
-  // const [jobURL, setJobURL] = useState([]);
+  const [jobData, setJobData] = useState<Job[]>();
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:8000/jobData/")
@@ -16,47 +19,48 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log(jobData);
+    console.log(jobData, "hi");
   });
-
-  function JobCard() {
-    return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Link href="#">Card Link</Card.Link>
-        </Card.Body>
-      </Card>
-    );
-  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Card border="light" bg="dark" style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Card Subtitle
-            </Card.Subtitle>
-            <Card.Link href="#">Card Link</Card.Link>
-          </Card.Body>
-        </Card>
+        <InputGroup size="lg" className="mb-3 w-50">
+          <InputGroup.Text
+            className="pe-0 bg-white rounded-end rounded-pill"
+            id="inputGroup-sizing-default"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </InputGroup.Text>
+          <Form.Control
+            className="border-start-0 shadow-none bg-white rounded-pill rounded-start"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <div className="d-flex justify-content-evenly flex-wrap">
+          {jobData?.map((job) => (
+            <Card border="light" bg="dark" style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title className="text-start">{job.company}</Card.Title>
+                <Card.Subtitle className="text-start mb-2 text-muted">
+                  {job.title}
+                </Card.Subtitle>
+                <Card.Link href={job.url}>Job Page</Card.Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ frontend/src/types/Job.ts
@@ -0,0 +1,6 @@
+export type Job = {
+  company: string;
+  title: string;
+  url: string;
+  date: string;
+};
