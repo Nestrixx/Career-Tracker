95dd2d568d16f065b882a0a3df884bda42d7127f
Christian McKinney <christian.mckinney@fdmgroup.com>
2024-05-27 00:52:56 -0500

    changed db.txt to a json file, upgraded to a modula

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,27 +1,28 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
 import fsPromises from "fs/promises";
 import path from "path";
 import cors from "cors";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = 8000;
+const dbPath = path.join(__dirname, "../", "db.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.get("/jobData", async (req: Request, res: Response) => {
-  const dbPath = path.join(__dirname, "../", "db.txt");
   const data = await fsPromises.readFile(dbPath, "utf8");
   return res.json(JSON.parse(data));
 });
 
 app.post("/addJob", async (req: Request, res: Response) => {
   req.body.date = new Date();
-  const dbPath = path.join(__dirname, "../", "db.txt");
   const oldDbText = await fsPromises.readFile(dbPath, "utf8");
   const newDbText = JSON.parse(oldDbText);
   newDbText.push(req.body);
